c709841d52e6775570855b41852c98ea6e43c26a
abu9hala99-maker <abu9hala99@gmail.com>
2026-09-13 22:33:30 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,6 @@
-const C='portfolio-v2';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest','./icon.svg']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+self.addEventListener("install",()=>self.skipWaiting());
+self.addEventListener("activate",e=>e.waitUntil((async()=>{
+  for(const k of await caches.keys()) await caches.delete(k);
+  await self.clients.claim();
+})()));
+self.addEventListener("fetch",e=>e.respondWith(fetch(e.request)));
